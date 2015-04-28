a9b4b552dbaabc295f20a9e3d582e4d939381f51
Mick Thompson <dthompson@gmail.com>
2015-04-28 12:40:47 -0700

    launch app from cli

--- bin/cli.js
+++ bin/cli.js
@@ -2,27 +2,64 @@
 var fs = require('fs');
 var dnode = require('dnode');
 var argv = require('minimist')(process.argv.slice(2));
-var d = dnode.connect(5004);
+var electron = require('electron-prebuilt')
+var proc = require('child_process')
+var net = require('net');
+
 
 //check to see if geojson.app is running. if not start it.
-d.on('remote', function (remote) {
-    if (!process.stdin.isTTY) {
+
+var startingApp = false;
+var retry = 0;
+
+function connect() {
+    net.connect(5004, function(err){
+        if(err) console.error(err);
+        remoteCommands();
+    }).on('error', function(e) {
+
+        if(!startingApp) {
+            var child = proc.spawn(electron, [__dirname+'/../'], {detached:true});
+            startingApp = true;
+            return setTimeout(connect, 400).unref();
+        }
+        if(retry > 50) {
+            return console.error('Cant connect to geojson.app', e);
+        }
+        retry++;
+        setTimeout(connect, 100).unref();
+    }).unref();
+
+}
+connect();
+
+function remoteCommands() {
+    var d = dnode.connect(5004);
+    d.on('remote', function (remote) {
+
         var data = '';
-        process.stdin.on('data', function(chunk){
-            data += chunk;
-        });
-        process.stdin.on('end', function(){
-            remote.open({data:data, clear:argv.clear}, function (s) {
+        if(argv._.length) {
+            data = fs.readFileSync(argv._[0], 'utf8');
+            remote.open({data:data, clear:argv.noclear}, function (s) {
                 d.end();
             });
-        });
-
-    } else {
-        if(argv._.length) {
-            var data = fs.readFileSync(argv._[0], 'utf8');
-            remote.open({data:data, clear:argv.clear}, function (s) {
-                d.end();
+        } else {
+            data = '';
+            var first = true
+            process.stdin.on('readable', function() {
+                var chunk = this.read();
+                if (chunk === null ) {
+                    if(!first && data === '') d.end();
+                } else {
+                   data += chunk;
+                }
+                first = false;
+            });
+            process.stdin.on('end', function() {
+                remote.open({data:data, clear:false}, function (s) {
+                    d.end();
+                });
             });
         }
-    }
-});
+    }).on('fail', console.error);
+}
--- src/main.js
+++ src/main.js
@@ -1,37 +1,93 @@
 // Load native UI library
 var fs = require('fs');
 var dnode = require('dnode');
-var mapboxjs = require('mapbox.js')
+var lightStyle = require('./style/mapbox-light.json');
+var geojsonExtent = require('geojson-extent');
+var geojsonNormalize = require('geojson-normalize');
+//var mapboxgl = require('mapbox-gl');
 var server = dnode({
     open : function (s, cb) {
-        if(s.clear) clear();
-        geojson(JSON.parse(s.data));
+        try {
+            geojson(JSON.parse(s.data), s.noClear);
+        } catch(e){
+            console.error('Error parsing geojson', e);
+        }
         cb();
     }
 }, {weak:false});
 server.listen(5004);
 
-L.mapbox.accessToken = 'pk.eyJ1IjoibWlja3QiLCJhIjoiLXJIRS1NbyJ9.EfVT76g4A5dyuApW_zuIFQ';
-var map = L.mapbox.map('map', 'mickt.hdof2a3d');
 var layers = [];
 
-function geojson(data){
 
-    var geojsonLayer = L.geoJson(data, {
-        onEachFeature: function (feature, layer) {
-            var desc = "";
-            for(p in feature.properties) {
-                desc += "<b>"+p+":</b> "+feature.properties[p]+"<br />";
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWlja3QiLCJhIjoiLXJIRS1NbyJ9.EfVT76g4A5dyuApW_zuIFQ';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: lightStyle,
+  center: [40, -74.50],
+  zoom: 9
+});
+
+var lineLayer;
+var markerLayer;
+
+function geojson(data, noClear){
+    if(!noClear) clear();
+    data = geojsonNormalize(data);
+    var bounds = geojsonExtent(data);
+
+    var geojsonSource = map.addSource('geojson', {
+        "type": "geojson",
+        "data": data
+    });
+
+
+    if(!lineLayer)
+        lineLayer = map.addLayer({
+            "id": "lines",
+            "type": "line",
+            "source": "geojson",
+            "paint": {
+                "line-color": "#EC8D8D",
+                "line-width": "4"
             }
-            layer.bindPopup(desc);
-        }
-    }).addTo(map);
-    layers.push(geojsonLayer);
-    map.fitBounds(geojsonLayer.getBounds());
+        });
+
+    if(!markerLayer)
+        markerLayer = map.addLayer({
+            "id": "markers",
+            "type": "symbol",
+            "source": "geojson",
+            "layout": {
+              "icon-image": "{marker-symbol}-12",
+              "text-field": "{title}{name}",
+              "text-font": "Open Sans Semibold, Arial Unicode MS Bold",
+              "text-offset": [0, 0.6],
+              "text-anchor": "top"
+            },
+            "paint": {
+              "text-size": 12
+            }
+        });
+
+    if ((bounds[1] ==bounds[3]) && (bounds[0] == bounds[2]))
+        return console.error('skipping bad bounds');
+
+    map.fitBounds([
+        [
+            bounds[1],
+            bounds[0]
+        ],
+        [
+            bounds[3],
+            bounds[2]
+        ]
+    ], {padding: 50, speed:3});
 }
 
 function clear(){
-    while(layers.length > 0) {
-        map.removeLayer(layers.shift());
-    }
+    try {
+        map.removeSource('geojson');
+    } catch(e){}
 }
